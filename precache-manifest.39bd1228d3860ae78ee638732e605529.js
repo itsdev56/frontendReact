7380eedb06b6b53be4d7e8e1f4205bef8cace23a
devraj <devrajs@valuefy.com>
2019-01-31 17:43:08 +0530

    Updates

---
+++ precache-manifest.39bd1228d3860ae78ee638732e605529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a190b67ac10e59e8cbca",
+    "url": "/frontendReact/static/js/runtime~main.a190b67a.js"
+  },
+  {
+    "revision": "e31409c228c13d4d327e",
+    "url": "/frontendReact/static/js/main.e31409c2.chunk.js"
+  },
+  {
+    "revision": "847b416e640960c5e946",
+    "url": "/frontendReact/static/js/1.847b416e.chunk.js"
+  },
+  {
+    "revision": "e31409c228c13d4d327e",
+    "url": "/frontendReact/static/css/main.dd0ac510.chunk.css"
+  },
+  {
+    "revision": "0debc6bade4804d6e2c7cda54e0c3f70",
+    "url": "/frontendReact/index.html"
+  }
+];
